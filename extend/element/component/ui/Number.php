71b952813258eda82207fc0e2993ef2d03887453
tangyijun <251784425@qq.com>
2021-12-01 17:44:31 +0800

    优化number 组件

--- extend/element/component/ui/Number.php
+++ extend/element/component/ui/Number.php
@@ -6,14 +6,17 @@ class Number
 {
     public static function html(array $fields,string $form_name = 'form'): string
     {
+        $min = $fields['prop']['ext']['min'] ?? 1;
+        $max = $fields['prop']['ext']['max'] ?? 999999;
+        $step = $fields['prop']['ext']['step'] ?? 1;
         return
-            '<el-form-item prop="'.$fields['key'].'" label="'.$fields['label'].'">'.PHP_EOL.
-                '<el-input-number '.PHP_EOL.
-                    ':min="'.$fields['prop']['ext']['min'].'" '.PHP_EOL.
-                    ':max="'.$fields['prop']['ext']['max'].'" '.PHP_EOL.
-                    ':step = "'.$fields['prop']['ext']['step'].'"'.PHP_EOL.
-                    'v-model="'.$form_name.'.'.$fields['key'].'">'.PHP_EOL.
-                '</el-input-number>'.PHP_EOL.
-            '</el-form-item>'.PHP_EOL;
+            '<el-form-item prop="' . $fields['key'] . '" label="' . $fields['label'] . '">' . PHP_EOL .
+            '<el-input-number ' . PHP_EOL .
+            ':min="' . $min . '" ' . PHP_EOL .
+            ':max="' . $max . '" ' . PHP_EOL .
+            ':step = "' . $step . '"' . PHP_EOL .
+            'v-model="' . $form_name . '.' . $fields['key'] . '">' . PHP_EOL .
+            '</el-input-number>' . PHP_EOL .
+            '</el-form-item>' . PHP_EOL;
     }
 }
